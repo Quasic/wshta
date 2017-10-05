56567ee77014aa7cd2b72c44c826d7cf8550ff37
@Quasic <qbasic010@aim.com>
2017-10-05 15:17:17 -0400

    missed a regex; entero replacing enter, but maintain backward compatibility for a while...; StackItem separates a large object from the called method by '\n.' for better readability than just '.'

--- require(module,console).js
+++ require(module,console).js
@@ -6,10 +6,10 @@ shims Array.prototype.indexOf
 This is more for reporting errors, such as to a file on a central server, rather than debugging, though it tries to give a good head start.
 
 in modules supporting submodule stack tracing, you can use a static or local shim function. The minimum would be:
-var enter=typeof console!=="undefined"&&console.enter||function(){return function(o){return o;};}
-enter will return an exit function, which should be called before returning, or as part of the return statement, {return exit(value)}, or assigned to an exit method of a thrown exception for the catching code to call if it doesn't call its own exit function.
+var entero=typeof console!=="undefined"&&console.entero||function(){return function(o){return o;};}
+entero will return an exit function, which should be called before returning, or as part of the return statement, {return exit(value)}, or assigned to an exit method of a thrown exception for the catching code to call if it doesn't call its own exit function.
 
-Functions which will not cause exceptions should not use console.enter.
+Functions which will not cause exceptions should not use console.entero.
 
 Copyright (C) Quasic of GitHub
 This program is free software: you can redistribute it and/or modify
@@ -25,10 +25,10 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 "use strict";module={
 //declared outside using only arguments object to avoid eval pollution
-js:function(/*h,module*/){var module=arguments[1];arguments.x=console.enter("require.extensions['.js']",arguments,{depth:0});arguments.v=module.id===module.filename&&require('fso').getBaseName(module.filename)||"*";console.enter(module.id);try{
+js:function(/*h,module*/){var module=arguments[1];arguments.x=console.entero(0,"require.extensions['.js']",arguments,{depth:0});arguments.v=module.id===module.filename&&require('fso').getBaseName(module.filename)||"*";console.entero(0,module.id);try{
 return arguments.x(module.returned=new Function("exports,require,module",arguments[0].readAll()+(arguments.v.search(require("module").R.vk)<0?'':";if(typeof "+arguments.v+"!=='undefined')return "+arguments.v+";"))(module.exports,function(n){return module.require(n);},module));
 }catch(e){console.trace(e,{},"JSEVALTRACE");throw e;}},
-trackEval:function(/*js,params,trace*/){arguments.exit=console.enter("trackEval",arguments,{object:this});console.enter(arguments[0],arguments[1]||[]);try{arguments.fn=new Function(arguments[0]);try{return arguments.exit(arguments.fn.apply({},arguments[1]||[]));}catch(e){if(arguments[2])console.trace(e,{},"Console.prototype.track::run","TRACKED ERROR");throw e;}}catch(e){if(arguments[2])console.trace(e,{},arguments[2],"Console.prototype.track::eval","TRACKED EVAL ERROR");else throw e;};}//for use by other language scripts, mostly. Use trackMethod, instead
+trackEval:function(/*js,params,trace*/){arguments.exit=console.entero(this,"trackEval",arguments);console.entero(0,arguments[0],arguments[1]||[]);try{arguments.fn=new Function(arguments[0]);try{return arguments.exit(arguments.fn.apply({},arguments[1]||[]));}catch(e){if(arguments[2])console.trace(e,{},"Console.prototype.track::run","TRACKED ERROR");throw e;}}catch(e){if(arguments[2])console.trace(e,{},arguments[2],"Console.prototype.track::eval","TRACKED EVAL ERROR");else throw e;};}//for use by other language scripts, mostly. Use trackMethod, instead
 };if(!Array.prototype.indexOf)Array.prototype.indexOf=function(obj,fromIndex){var o=Object(this),len=o.length>>>0,n,k;if(!len||(n=Math.ceil(Math.abs(fromIndex))||0)>=len)return-1;for(k=n<0?Math.max(len-Math.abs(n),0):n;k<len;k++)if(k in o&&o[k]===obj)return k;return-1;};//used in stringFrom and module code
 (function(){var
 OptS=Object.prototype.toString,
@@ -78,10 +78,10 @@ if(wsh)try{WScript.stdout.write("");stderr=WScript.stderr;csh=true;}catch(e){WSc
 var cache={},extensions={
 ".js":module.js,
 ".json":typeof JSON==="object"?function(h,module){var
-x=console.enter("require.extensions['.json']",arguments,{depth:0});
+x=console.entero(0,"require.extensions['.json']",arguments,{depth:0});
 module.exports=JSON.parse(h.readAll());
 }:function(h,module){var
-x=console.enter("require.extensions['.json']",arguments,{depth:0}),
+x=console.entero(0,"require.extensions['.json']",arguments,{depth:0}),
 n=R.trim,
 c,
 d=null,
@@ -90,8 +90,8 @@ if(e&&!e.replace(/(,)|(\[|{)|(}|])|"(?:[^"\\\r\n]|\\["\\\/bfnrt]|\\u[\da-fA-F]{4
 throw new Error("Invalid JSON: "+e);}//this function based on jQuery's from version 1.11.2, 2014 jQuery Foundation, Inc. [jquery.org/license]
 };
 mainpath=wsh?WScript.scriptFullName:win?window.location.href.search(/^file:\/\/\/[A-Z]:/)>=0&&fso.fileexists(unescape(window.location.href).substring(8))&&fso.getFile(unescape(window.location.href).substring(8)).path||fso.fileexists(unescape(window.location.href).substring(5))&&fso.getFile(unescape(window.location.href).substring(5)).path||"[Eval]":"[Eval]";
-if(wsh&&WScript.scriptFullName.search(fmr)>=0)thispath=WScript.scriptFullName;
-else if(win)for(var i=0;i<document.scripts.length;i++){if(document.scripts[i].src.search(fmr)>=0)thispath=fso.getFile(document.scripts[i].src).path;else cacheWinScript(i);}
+if(wsh&&WScript.scriptFullName.search(R.fm)>=0)thispath=WScript.scriptFullName;
+else if(win)for(var i=0;i<document.scripts.length;i++){if(document.scripts[i].src.search(R.fm)>=0)thispath=fso.getFile(document.scripts[i].src).path;else cacheWinScript(i);}
 if(!thispath)for(var i=fso.getFolder(".");true;i=i.parentFolder){if(fso.fileexists(i.path+"/"+thisfilename))thispath=fso.getFile(i.path+"/"+thisfilename).path;if(i.isRootFolder)break;}
 if(!thispath)thispath=thisfilename;
 	//Utility objects and functions
@@ -151,8 +151,8 @@ if(opt.recursiveList.indexOf(o)>=0)return"[Circular "+s.substring(8);
 opt.recursiveList.push(o);
 t={Console:Console,Module:Module};
 for(i in t)if(o instanceof t[i])s="["+i+"]";
-var x={depth:0};if(this instanceof Console)x.object=this;
-x=console.enter("stringFrom",arguments,x);
+var x={depth:0};
+x=console.entero(this instanceof Console&&this,"stringFrom",arguments,x);
 if((typeof o.inspect==="function"||OptS.apply(o.inspect)==='[object Function]')&&opt.customInspect)return x(o.inspect(opt.depth,Opt,opt));
 	//no more returns before x()
 	i=1;
@@ -182,24 +182,28 @@ return x((s==="[Object]"?'{'+t.join(',\n')+'}':s+(t.length?'{'+t.join(',\n')+'}'
 stringFrom.host=function(o){return OptS.apply(o);}
 	//Classes
 function functionDescriptor(f){var s=f+'',i=s.indexOf(')');return i<2?s:s.substring(0,i+1);}
-//Do not use enter, or any functions using enter, in the StackItem constructor!
-function StackItem(n,p,O){this.n=n;this.p=p;if(typeof O==="object"){
+//Do not use entero, or any functions using entero, in the StackItem constructor!
+function StackItem(o,n,p,O){this.n=n;this.p=p;if(typeof O==="object"){
 this.O=O;
-if(O.object)this.obj=O.object;
+this.obj=o;
 if(O.deprecated)console.warn(this+" is deprecated. "+O.deprecated);
 }else this.O={};
 if(!(''+n))console.trace("Empty stack name for "+this);
 }
-StackItem.prototype.toString=function(){var p=[],i;
+StackItem.prototype.toString=function(){var
+p=[],
+i,
+o=this.obj?stringFrom(this.obj,this.obj instanceof Module||this.obj instanceof Console?{depth:0}:this.O):'';
 if(this.p&&this.p.length)for(i=0;i<this.p.length;i++)p[i]=stringFrom(this.p[i],this.O);
-return((this.obj?stringFrom(this.obj,this.obj instanceof Module||this.obj instanceof Console?{depth:0}:this.O)+'.':'')
+return(o
++(o.length>45||o.indexOf("\n")>=0?'\n.':'.')
 +this.n+(this.p?'('+p.join(',')+')':'')
 +(this.O.deprecated?'[deprecated]':'')
 +(this.p&&this.p.callee?" as "+functionDescriptor(this.p.callee):'')
 //caller empty outside of function
 );};
 var stack=[],tc={};
-function Console(out,err){var x=console.enter("require('console').Console",arguments);
+function Console(out,err){var x=console.entero(0,"require('console').Console",arguments);
 this.stream=[out,err||out];
 if(typeof out.writeLine==="undefined")throw x(new Error("out stream can't writeLine: "+this.stringFrom(out)));
 if(typeof this.stream[1].writeLine==="undefined")throw new Error("err stream can't writeLine: "+this.stringFrom(err));x();}
@@ -210,15 +214,17 @@ Console.prototype.stringFrom=stringFrom;
 Console.prototype.functionDescriptor=functionDescriptor;
 function stringTime(d){return d.toString().replace(R.fr,function(a,b){return":"+(+b+100+(d%1000)/1000).toString().substring(1)+" ";});}
 Console.prototype.formatLog=function(k,msg,options,type,trace){
-var i=stack.length-1,x=console.enter("formatLog",arguments,{object:this}),O=options||{},d=new Date,s=O.from?O.from||"O.from":i<0?arguments.caller||stack[i+1]||":/":stack[i--]||"???";
+var d=new Date,i=stack.length-1,x=console.entero(this,"formatLog",arguments),O=options||{},s=O.from?O.from||"O.from":i<0?arguments.caller||stack[i+1]||":/":stack[i--]||"???";
 if(trace||O.trace)for(;i>=0;i--)s+="\n\t...in "+stack[i];
 //arguments.caller.caller is always empty with caller=null, so can't use it to trace very far at all, useless here, by itself (caller empty outside of function)
-s=stringTime(d)+(type?" "+type+": ":' ')+this.stringFrom(msg,options)+" From "+s+"\n"+stringTime(i=new Date)+' '+(i-d)/1000+"s";
+s=stringTime(d)+" "+(cache.wshNet?cache.wshNet.exports.username+'@'+(cache.wshNet.exports.userdomain!==cache.wshNet.exports.computername?cache.wshNet.exports.userdomain+'@':cache.wshNet.exports.computername)+' ':'')+(type?type+": ":' ')+this.stringFrom(msg,options)+" From "+s+"\n"+stringTime(i=new Date)+' '+(i-d)/1000+"s";
 try{
 this.stream[k].writeLine(s);
 }catch(e){stderr.writeLine("Error logging to stream["+k+"]="+this.stringFrom(this.stream[k])+": "+this.stringFrom(e)+" Original error: "+s);}
 return x(d);};
-Console.prototype.enter=function(n,params,O){var
+Console.prototype.enter=function(n,params,O){return this.entero(O&&O.object,n,params,O);};//almost deprecated...
+Console.prototype.enteroAnyway=function(object,n,params,O){stack.oops=1;this.entero(object,n,params,O);};//allows calling entero in an onExit, but use cautiously
+Console.prototype.entero=function(object,n,params,O){var
 d=O||{depth:1},
 c=this,
 b=c.verbose||d.verbose,
@@ -233,12 +239,11 @@ f.resume=function(e,O){
 if(e)c.formatLog(1,e,O||d,"RESUME_TRACE",1);
 if(b)c.formatLog(0,e||"No error given",O||d,"RESUME@stack["+x+"] "+stack[x]+" from stack["+(stack.length-1)+"] "+stack[stack.length-1]);
 stack.length=x+1;return e;};
-stack[x]=new StackItem("enter",arguments,d);
-stack[x]=new StackItem(n,params,d);
+stack[x]=new StackItem(c,"entero",arguments,d);
+stack[x]=new StackItem(object,n,params,d);
 if(b)c.stream[0].writeLine(stringTime(new Date)+" ENTER@stack["+x+"] "+stack[x]+" verbosity "+(d.verbose?"local":"global"));
 return f;
 };
-Console.prototype.enterAnyway=function(n,params,O){stack.oops=1;this.enter(n,params,O);};//allows calling enter in an onExit, but use cautiously
 Console.prototype.trace=function(msg,options,type){this.formatLog(1,msg,options,type||"TRACE",1);};
 Console.prototype.log=function(msg,options){this.formatLog(0,msg,options,"LOG");};
 Console.prototype.dir=function(o,options){this.formatLog(0,o,options,"INSPECT");};
@@ -251,12 +256,12 @@ Console.prototype.throwifnot=Console.prototype.assert=function(test,msg){if(test
 Console.prototype.logifnot=function(test,msg){if(test)return;this.log(msg);};//~browser version of assert
 Console.prototype.logif=function(test,msg){if(test)this.log(msg);};
 Console.prototype.logThru=function(o){this.log(o);return o;}
-Console.prototype.track=function(fn,params,trace){var x=console.enter("track",arguments,{object:this,deprecated:"Use trackMethod, instead, as this makes logs longer"});console.enter(fn,params||[]);try{return x(fn.apply({},params||[]));}catch(e){if(trace)console.trace(e,{},trace,"Console.prototype.track::apply","TRACKED ERROR");else throw e;}x();};
-Console.prototype.trackMethod=function(object,method,params,trace){var x=console.enter("track",arguments,{object:this});console.enter(method,params,{object:object});try{return x(object[method].apply(object,params||[]));}catch(e){if(trace)console.trace(e,{},"Console.prototype.trackMethod::apply","TRACKED METHOD ERROR");else throw e;}x();}
+Console.prototype.track=function(fn,params,trace){var x=console.entero(this,"track",arguments,{deprecated:"Use trackMethod, instead, as this makes logs longer"});console.entero(0,fn,params||[]);try{return x(fn.apply({},params||[]));}catch(e){if(trace)console.trace(e,{},trace,"Console.prototype.track::apply","TRACKED ERROR");else throw e;}x();};
+Console.prototype.trackMethod=function(object,method,params,trace){var x=console.entero(this,"track",arguments);console.entero(object,method,params);try{return x(object[method].apply(object,params||[]));}catch(e){if(trace)console.trace(e,{},"Console.prototype.trackMethod::apply","TRACKED METHOD ERROR");else throw e;}x();}
 Console.prototype.trackEval=module.trackEval;
 if(typeof console!=="undefined")Console.host=console;
 if(mainpath!=thispath)stack[0]=new StackItem(mainpath);
-console={enter:function(){return function(o){return o;};}};//shim for next line
+console={entero:function(){return function(o){return o;};}};//shim for next line
 console=csh?new Console(WScript.stdout,stderr):new Console({writeLine:function(s){var h,m=s;try{
 h=fso.openTextFile("console.log",8,1);
 }catch(e){
@@ -264,11 +269,11 @@ m="ERROR: "+stringFrom(e)+" Unable to log: "+m;
 h=stderr;
 }h.writeLine(m);h.close();}},stderr);
 try{
-if(win)window.onerror=function(m,u,l){var x=console.enter(unescape(u)+"<"+l+">");console.enter("window.onerror:console",arguments);console.trace(m,{},"UNCAUGHT ERROR");return x(console.cont);};
-var X=console.enter(thispath);
+if(win)window.onerror=function(m,u,l){var x=console.entero(0,unescape(u)+"<"+l+">");console.entero(0,"window.onerror:console",arguments);console.trace(m,{},"UNCAUGHT ERROR");return x(console.cont);};
+var X=console.entero(0,thispath);
 function Module(id,exports){cache[this.id=id]=this;this.loaded=false;this.exports=exports||{};this.filename=id;this.parents=[];this.children=[];}
-Module.prototype.create=function(n,exports){var x=console.enter("create",arguments,{object:this}),m=new Module(n,exports);m.filename=this.filename;if(m.parents.indexOf(this)<0){m.parents.push(this);this.children.push(this);}return x(m);};
-Module.prototype.require=function(n){var x=console.enter("require",arguments,{object:this}),f=this.resolve(n);
+Module.prototype.create=function(n,exports){var x=console.entero(this,"create",arguments),m=new Module(n,exports);m.filename=this.filename;if(m.parents.indexOf(this)<0){m.parents.push(this);this.children.push(this);}return x(m);};
+Module.prototype.require=function(n){var x=console.entero(this,"require",arguments),f=this.resolve(n);
 if(!f)throw new Error('Module "'+n+'" not found from '+this.filename);
 var b={},m=cache[f]/*&&cache[f].ts==f.dateLastModified&&cache[f]*/||axo.hasOwnProperty(f)&&(new Module(f,new ActiveXObject(axo[f])).loaded=true)&&cache[f]||new Module(f,b);
 if(m.exports===b||m.parents.indexOf(this)<0){this.children.push(m);m.parents.push(this);}
@@ -282,14 +287,14 @@ for(h in m.exports)if(m.exports.hasOwnProperty(h)){b=!m.exports;break;}
 if(m.exports===b)m.exports=y;
 }
 return x(m.exports);};
-function mRequire(m){var r=function(n){return console.enter(":require",arguments,{object:m})(m.require(n));};
+function mRequire(m){var r=function(n){return console.entero(m,":require",arguments)(m.require(n));};
 r.cache=cache;
 r.extensions=extensions;
 r.resolve=function(n){return m.resolve(n);};
 r.main=main;
 r.requireFrom=arguments.callee;
 return r;}
-Module.prototype.resolve=function(n,debug){var x=console.enter("resolve",arguments,{object:this}),i;
+Module.prototype.resolve=function(n,debug){var x=console.entero(this,"resolve",arguments),i;
 if(debug)console.log('resolving "'+n+'"');
 if(win)for(i=0;i<document.scripts.length;i++)cacheWinScript(i);
 if(cache.hasOwnProperty(n)||axo.hasOwnProperty(n)){if(debug)console.log("from cache");return x(n);}
@@ -336,7 +341,7 @@ Q+="; cache:";
 for(q in cache)Q+=' "'+q+'"'+(cache[q].loaded?'':"(loading)");
 Q+="; ActiveX modules:";
 for(q in axo)Q+=' '+q;
-return Q;},create:function(n,exports){return console.enter("require('module').create",arguments)(new Module(n,exports));},extensions:extensions,R:R,stringTime:stringTime}).loaded=true;
+return Q;},create:function(n,exports){return console.enter(0,"require('module').create",arguments)(new Module(n,exports));},extensions:extensions,R:R,stringTime:stringTime}).loaded=true;
 require=mRequire(module);
 }catch(e){console.trace(e,{},"ERRORINbase");throw e;}
 if(wsh&&WScript.scriptFullName.search(R.fm)>=0){
@@ -350,7 +355,7 @@ require("./debugConsole.js")();
 module.loaded=true;
 module=main;
 if(win)require=function(n){//special handler for script tags, though not quite as good (must use require before the module object to make sure it's the right module object)
-var x=console.enter("require",arguments),
+var x=console.entero(0,"require",arguments),
 i;
 for(i=0;i<document.scripts.length;i++)cacheWinScript(i);
 return x(module.require(n));
